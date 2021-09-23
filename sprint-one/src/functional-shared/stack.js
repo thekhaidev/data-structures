a7d269a16fc5c6716d358c2701e5f8ec72df514f
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-23 15:40:12 -0700

    Progress functional-shared stack code

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -10,29 +10,30 @@ var Stack = function() {
   //start size at 0
   someInstance.size = 0;
 
-  _.extend(someInstance, stackMethods);
-
+  extend(someInstance, stackMethods);
+  // console.log(someInstance)
   return someInstance;
 };
 
 // this === someInstance (each of which are unique)
 
 var stackMethods = {
-  // //push add cargo
-  // var push = function(value){
-  //   this.storageObj[this.key] = value; //equivalent to line 22 of functional stack
+  //push add cargo
+  // push : function(value){
+  //   //equivalent to line 22 of functional stack
+  //   this.storageObj[this.key] = value;
   //   this.key++;
   //   return this.storageObj;
   // };
-  // //pop take out cargo
-  // var pop = function () {
+  //pop take out cargo
+  // pop : function () {
   //   var last = this.storageObj[this.key -1];
   //   delete this.storageObj[this.key -1];
   //   this.key--;
   //   return last;
   // };
-  // // //size how much cargo is in the hold
-  // var size = function () {
+  // //size how much cargo is in the hold
+  // size : function () {
   //   this.size = Object.keys(storage.length);
   //   return this.size;
   // };
