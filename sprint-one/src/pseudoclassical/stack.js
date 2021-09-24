132ebddc899f118494e47a9f1250cc6a9f708fb4
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-24 16:06:21 -0700

    Implemented working code for Pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //properties
+  this.storage = {};
+  this.counter = 0;
 };
 
 
+//push
+Stack.prototype.push = function(value) {
+  // add value to top of stack
+  this.storage[this.counter] = value;
+  //increment counter
+  this.counter++;
+};
+
+//pop
+Stack.prototype.pop = function() {
+  // decrement the counter
+  this.counter--;
+  // assign the last value to a variable
+  var last = this.storage[this.counter];
+  // delete the last value
+  delete this.storage[this.counter];
+  //return the variable
+  return last;
+};
+
+//size
+Stack.prototype.size = function() {
+  //return the size
+  return Object.keys(this.storage).length;
+};
+
+
+//new instance
+var NewInstance = new Stack();
