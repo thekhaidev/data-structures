f8efceaaefe4baedf8198cf901f0e15ea98d23c8
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-24 15:13:32 -0700

    Implemented working code for Prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create an instance
+  // storage property assigned to  created storage object
+
+  //add the properties to the storage property
+
+  //return the instance
+
+  var anInstance = Object.create(stackMethods);
+  anInstance.storage = {};
+  anInstance.counter = 0;
+  return anInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  //push
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
 
+  //pop
+  pop: function() {
+    this.counter--;
+    var lastValue = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return lastValue;
+  },
+
+  //size
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
