120cf7b90a76389dea3c17c49eaa1ca49914dd75
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-23 17:33:54 -0700

    Progressed code for funtional-shared stack and began code for queue

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,42 +1,43 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  //hull of the boat (holds all other parts)
-  var someInstance = {};
-  //cargo hold
-  someInstance.storageObj = {};
-  //numerical key value
-  someInstance.key = 1;
-  //start size at 0
-  someInstance.size = 0;
+// var Stack = function() {
 
-  extend(someInstance, stackMethods);
-  // console.log(someInstance)
-  return someInstance;
-};
+//   //hull of the boat (holds all other parts)
+//   var someInstance = {};
+//   //cargo hold
+//   someInstance.storageObj = {};
+//   //numerical key value
+//   someInstance.key = 1;
+//   //start size at 0
+//   someInstance.size = 0;
 
-// this === someInstance (each of which are unique)
+//   extend(someInstance, stackMethods);
 
-var stackMethods = {
-  //push add cargo
-  // push : function(value){
-  //   //equivalent to line 22 of functional stack
-  //   this.storageObj[this.key] = value;
-  //   this.key++;
-  //   return this.storageObj;
-  // };
-  //pop take out cargo
-  // pop : function () {
-  //   var last = this.storageObj[this.key -1];
-  //   delete this.storageObj[this.key -1];
-  //   this.key--;
-  //   return last;
-  // };
-  // //size how much cargo is in the hold
-  // size : function () {
-  //   this.size = Object.keys(storage.length);
-  //   return this.size;
-  // };
-};
+//   return someInstance;
+// };
 
+// // this === someInstance (each of which are unique)
+// var stackMethods = {};
 
+//   push add cargo
+//   stackMethods.push = function(value){
+//     //equivalent to line 22 of functional stack
+//     //someInstance.storageObj[someInstance.key] = value
+//     this.storageObj[this.key] = value;
+//     //someInstance.key++
+//     this.key++;
+//     //return someInstance.storageObj
+//     return this.storageObj;
+//   };
+
+//   // pop take out cargo
+//   stackMethods.pop = function () {
+//     var last = this.storageObj[this.key -1];
+//     delete this.storageObj[this.key -1];
+//     this.key--;
+//     return last;
+//   };
+
+//   //size how much cargo is in the hold
+//   stackMethods.size = function () {
+//     this.size = Object.keys(this.storageObj.length);
+//     return this.size;
+//   };
