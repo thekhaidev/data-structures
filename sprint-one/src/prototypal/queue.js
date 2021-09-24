1fc8011c15c0aec6e2d1bd586693fe28d04b1704
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-24 15:30:18 -0700

    Implemented functional code for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //create the instance
+  //assign properties
+  //storage
+  //count
+
+  //return instance
+
+  var anInstance = Object.create(queueMethods);
+
+  anInstance.storage = {};
+  anInstance.counter = 0;
+
+  return anInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //enqueue
+  enqueue: function(value) {
+    if (this.storage[this.counter]) {
+      this.counter++;
+      this.storage[this.counter] = value;
+    } else {
+      this.storage[this.counter] = value;
+      this.counter++;
+    }
+  },
+  //dequeue
+  dequeue: function() {
+    var removed = this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    this.counter--;
+    return removed;
+  },
+
+  //size
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+
+};
 
 
