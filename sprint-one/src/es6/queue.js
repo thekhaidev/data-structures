35f0cbe501ff688d9b0b3088cfeb66e477b6e677
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-24 19:19:19 -0700

    Implemented working code for es6 queue and stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,31 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  enqueue(value) {
+    //add to the end
+    if (this.storage[this.counter]) {
+      this.counter++;
+      this.storage[this.counter] = value;
+    } else {
+      this.storage[this.counter] = value;
+      this.counter++;
+    }
+  }
+
+  dequeue() {
+    //remove from the front
+    var first = this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    this.counter--;
+    return first;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var result = new Giraffe();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+
+  pop() {
+    this.counter--;
+    var popped = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return popped;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var placeholder = new Stack();
