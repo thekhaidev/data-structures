0d45eff70f9bd4c2a5c805f813bb669f9dfb313b
Rudolf Meyers <rudolf.a.meyers@gmail.com>
2021-09-24 20:30:02 -0700

    Progressed code for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // take the value
+    // create a new node with that value
+    // if there is no head --> become the head
+    // if there is a head --> become tail
+    // if there is no tail --> tail
+
+    var newNode = new Node(value);
+
+    if (this.tail) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail = newNode;
+
   };
 
   list.removeHead = function() {
+    // node.next --> head
+    // if this.head exists --> remove head
+
   };
 
   list.contains = function(target) {
+    // iterate through linked list for target
+    // return true/false
   };
 
   return list;
